428c6f805923fa4640c30480de4aeb4ad50da48f
kkernan <kkernan@gmail.com>
2024-06-16 06:23:15 -0500

    Add OH58D items

---
+++ CoordinateConverter/DCS/Aircraft/OH58D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+namespace CoordinateConverter.DCS.Aircraft
+{
+    public class OH58D: DCSAircraft
+    {
+
+        protected override List<DCSCommand> GetPreActions()
+        {
+            throw new System.NotImplementedException();
+        }
+        protected override List<DCSCommand> GetActions(object item)
+        {
+            throw new System.NotImplementedException();
+        }
+        protected override List<DCSCommand> GetPostActions()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override List<string> GetPointTypes()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override List<string> GetPointOptionsForType(string pointTypeStr)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
